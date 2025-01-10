a224f2395f970c7899d3c1fe26bb9a2c6b081428
Steven Fields <stevenjfields97@gmail.com>
2025-01-09 21:33:15 -0600

    take list of string, output list of embeddings

--- app/api/embed_api.py
+++ app/api/embed_api.py
@@ -8,17 +8,17 @@ from models.embed import EmbedRequest
 router = APIRouter()
 
 @router.post("/embed")
-def embed(request: fastapi.Request, body: EmbedRequest) -> list[float]:
+def embed(request: fastapi.Request, body: list[str]) -> list[list[float]]:
     embedding_model = request.scope["embedding_model"]
     tokenizer = request.scope["tokenizer"]
     vector_linear = request.scope["vector_linear"]
     with torch.no_grad():
-        input_data = tokenizer(body.text, padding="longest", truncation=True, max_length=512, return_tensors="pt")
+        input_data = tokenizer(body, padding="longest", truncation=True, max_length=512, return_tensors="pt")
         input_data = {k: v.cuda() for k, v in input_data.items()}
         attention_mask = input_data["attention_mask"]
         last_hidden_state = embedding_model(**input_data)[0]
         last_hidden = last_hidden_state.masked_fill(~attention_mask[..., None].bool(), 0.0)
-        text_embedding = last_hidden.sum(dim=1) / attention_mask.sum(dim=1)
-        text_embedding = normalize(vector_linear(text_embedding).cpu().numpy())
-    return text_embedding[0]
+        text_embeddings = last_hidden.sum(dim=1) / attention_mask.sum(dim=1)[..., None]
+        text_embeddings = normalize(vector_linear(text_embeddings).cpu().numpy())
+    return text_embeddings
 

0b02331ab3f29e4eb7df9d9c49e6578059bc1914
Steven Fields <stevenjfields97@gmail.com>
2025-01-09 21:28:42 -0600

    embed single item instead of list

--- app/api/embed_api.py
+++ app/api/embed_api.py
@@ -13,12 +13,12 @@ def embed(request: fastapi.Request, body: EmbedRequest) -> list[float]:
     tokenizer = request.scope["tokenizer"]
     vector_linear = request.scope["vector_linear"]
     with torch.no_grad():
-        input_data = tokenizer([body.text], padding="longest", truncation=True, max_length=512, return_tensors="pt")
+        input_data = tokenizer(body.text, padding="longest", truncation=True, max_length=512, return_tensors="pt")
         input_data = {k: v.cuda() for k, v in input_data.items()}
         attention_mask = input_data["attention_mask"]
         last_hidden_state = embedding_model(**input_data)[0]
         last_hidden = last_hidden_state.masked_fill(~attention_mask[..., None].bool(), 0.0)
-        text_embedding = last_hidden.sum(dim=1) / attention_mask.sum(dim=1)[..., None]
+        text_embedding = last_hidden.sum(dim=1) / attention_mask.sum(dim=1)
         text_embedding = normalize(vector_linear(text_embedding).cpu().numpy())
     return text_embedding[0]
 
